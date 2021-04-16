d9bc6833a16029b0d5a09c32962265e7b05e3401
Хайрбек Мамбетов <mnocard@gmail.com>
2021-04-16 21:28:15 +0500

    Контроллер Values

---
+++ Services/WebStore.ServiceHosting/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly List<string> _Values = Enumerable
+            .Range(1, 10)
+            .Select(i => $"Value {i:00}")
+            .ToList();
+
+        [HttpGet]
+        public IEnumerable<string> Get() => _Values;
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= _Values.Count)
+                return NotFound();
+
+            return _Values[id];
+        }
+
+        [HttpPost("add")]
+        public ActionResult Post([FromBody]string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return BadRequest();
+
+            _Values.Add(value);
+            return CreatedAtAction(nameof(Get), new { Id = _Values.Count - 1 });
+        }
+
+        [HttpPut("edit/{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            if (id < 0 || string.IsNullOrEmpty(value))
+                return BadRequest();
+            if (id >= _Values.Count)
+                return NotFound();
+
+            _Values[id] = value;
+            return Ok();
+        }
+
+        [HttpDelete("remove/{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0)
+                return BadRequest();
+            if (id >= _Values.Count)
+                return NotFound();
+
+            _Values.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -9,14 +9,10 @@ namespace WebStore.ServiceHosting
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -27,7 +23,6 @@ namespace WebStore.ServiceHosting
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
