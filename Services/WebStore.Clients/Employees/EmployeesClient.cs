c94ca85ff0fea6b404eb72eb92b87805a487e4a0
Хайрбек Мамбетов <mnocard@gmail.com>
2021-04-18 10:05:19 +0500

    Клиент для работы с сотрудниками через Апи

--- Services/WebStore.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using Microsoft.Extensions.Configuration;
 using WebStore.Clients.Base;
 using WebStore.Domain.Models;
@@ -11,15 +13,21 @@ namespace WebStore.Clients.Employees
     {
         public EmployeesClient(IConfiguration config) : base (config, WebApi.Employees) { }
 
-        public IEnumerable<Employee> Get()
-        {
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
 
-        }
-        public Employee Get(int id) => throw new System.NotImplementedException();
-        public Employee GetByName(string LastName, string FirstName, string Patronymic) => throw new System.NotImplementedException();
-        public int Add(Employee employee) => throw new System.NotImplementedException();
-        public Employee Add(string LastName, string FirstName, string Patronymic, int Age) => throw new System.NotImplementedException();
-        public void Update(Employee employee) => throw new System.NotImplementedException();
-        public bool Delete(int id) => throw new System.NotImplementedException();
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
+
+        public Employee GetByName(string LastName, string FirstName, string Patronymic) =>
+            Get<Employee>($"{Address}/employee?LastName={LastName}&FirstName={FirstName}&Patronymic={Patronymic}");
+
+        public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
+
+        public Employee Add(string LastName, string FirstName, string Patronymic, int Age) =>
+            Post($"{Address}/employee?LastName={LastName}&FirstName={FirstName}&Patronymic={Patronymic}", "").Content.ReadAsAsync<Employee>().Result;
+
+
+        public void Update(Employee employee) => Put(Address, employee);
+
+        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
     }
 }
