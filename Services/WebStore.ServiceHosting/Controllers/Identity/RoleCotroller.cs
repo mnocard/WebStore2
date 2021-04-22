a1a67ef0bb71245898b70c7677779831815dc419
Хайрбек Мамбетов <mnocard@gmail.com>
2021-04-22 14:52:33 +0500

    Заготовки контроллеров Identity

---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/RoleCotroller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebApi.Identity.Roles)]
+    [ApiController]
+    public class RoleCotroller : ControllerBase
+    {
+        private readonly RoleStore<Role> _RoleStore;
+        public RoleCotroller(WebStoreDB Db)
+        {
+            _RoleStore = new RoleStore<Role>(Db); 
+        }
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/UsersCotroller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebApi.Identity.Users)]
+    [ApiController]
+    public class UsersCotroller : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDB> _UserStore;
+
+        public UsersCotroller(WebStoreDB Db)
+        {
+            _UserStore = new UserStore<User, Role, WebStoreDB>(Db);
+        }
+    }
+}
