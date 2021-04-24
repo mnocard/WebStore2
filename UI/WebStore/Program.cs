dd679237662ddb51c5401ef4770c4b77e691d01a
Хайрбек Мамбетов <mnocard@gmail.com>
2021-04-24 21:43:52 +0500

    Подключение Serilog

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+using System;
 
 namespace WebStore
 {
@@ -21,6 +25,15 @@ namespace WebStore
                 //)
                 .ConfigureWebHostDefaults(host => host
                    .UseStartup<Startup>()
+                )
+                .UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+                   .MinimumLevel.Debug()
+                   .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                   .Enrich.FromLogContext()
+                   .WriteTo.Console(
+                        outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+                   .WriteTo.RollingFile($@".\Log\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+                   .WriteTo.File(new JsonFormatter(",", true), $@".\Log\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
                 )
             ;
     }
