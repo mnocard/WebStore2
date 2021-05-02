9717f31d2b99135ed5b667ae7694d143083fc6d5
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-02 14:08:01 +0500

    Заготовка таг-хелпера

---
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement(Attributes = AttributeName)]
+    public class ActiveRoute :TagHelper
+    {
+        private const string AttributeName = "is-active-route";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll(AttributeName);
+        }
+    }
+}
