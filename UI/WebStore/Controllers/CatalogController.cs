9dc0109e8286b2869379c7111b39765b58e616da
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-08 13:17:57 +0500

    Подготовка разметки страниц каталога товаров

--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using WebStore.Domain;
@@ -10,6 +11,8 @@ namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
+        private const string __CatalogPageSize = "CatalogPageSize";
+
         private readonly IProductData _ProductData;
         private readonly IConfiguration _Configuration;
 
@@ -22,7 +25,7 @@ namespace WebStore.Controllers
         public IActionResult Index(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
         {
             var page_size = PageSize
-                ?? (int.TryParse(_Configuration["CatalogPageSize"], out var value) ? value : null);
+                ?? (int.TryParse(_Configuration[__CatalogPageSize], out var value) ? value : null);
 
             var filter = new ProductFilter
             {
@@ -58,5 +61,27 @@ namespace WebStore.Controllers
 
             return View(product.FromDTO().ToView());
         }
+
+        #region WebAPI
+        public IActionResult GetFeaturesItems(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
+        {
+            return PartialView("Partial/_FeaturesItems", GetProducts(BrandId, SectionId, Page, PageSize));
+        }
+
+        private IEnumerable<ProductViewModel> GetProducts(int? BrandId, int? SectionId, int Page, int? PageSize)
+        {
+            return _ProductData.GetProducts(new ProductFilter
+            {
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Page = Page,
+                PageSize = PageSize ?? (int.TryParse(_Configuration[__CatalogPageSize], out var size) ? size : null),
+            })
+                .Products.OrderBy(p => p.Id)
+                .FromDTO()
+                .ToView();
+        }
+
+        #endregion
     }
 }
